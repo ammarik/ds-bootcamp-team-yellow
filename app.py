69b6bda2d617fa8d23c0f007bf72e0e8e3bf5e73
Ellen Hoeven <ellen.hoeven@ibm.com>
2021-11-15 15:31:24 +0100

    feat: add the first version of the app with a hello world page

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Streamlit hello world page
+"""
+
+#import the library
+import streamlit as stl
+
+# add title to your app
+stl.title("Movie recommender")
+
+#adding text to your app
+stl.write("Hello user! Movie recommendations coming soon :-) ")
