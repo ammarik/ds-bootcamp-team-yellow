7b8670cb041a77f017a51589c509b51f4c1c84ab
Marek Marik <marek.marik@ibm.com>
2021-11-18 07:38:01 +0100

    Show movie info for new usr. Error handling in movie info retrieval.

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from movie_source import MovieInfoSource
+from movie_source import FailedToRetrieveMovieInformation
--- src/movie_source.py
+++ src/movie_source.py
@@ -1,11 +1,26 @@
 """Provide more information about the given film."""
 import csv
+import logging
 from io import BytesIO
 from urllib.request import urlopen
 
 import requests
 from lxml import etree
 from PIL import Image
+
+from utils.get_logger import get_logger
+
+logger: logging.Logger = get_logger()
+
+
+class FailedToRetrieveMovieInformation(Exception):
+    """
+    The exception that is raised when it fails to get information about the film.
+    """
+
+    def __init__(self, movie_id):
+        self.message = f'Failed to retrieve information about movie {movie_id}'
+        super().__init__(self.message)
 
 
 class MovieInfoSource:
@@ -30,7 +45,12 @@ class MovieInfoSource:
         Based on the given movie ID, this method returns name 
         of the movie
         """
-        return self.movies_info[movie_id][0]
+        try:
+            movie_name = self.movies_info[movie_id][0]
+        except IndexError:
+            logger.warning(f'Failed to get movie name - Unknown movie_id: {movie_id}')
+            raise FailedToRetrieveMovieInformation(movie_id)
+        return movie_name
 
     def get_movie_info(self, movie_id):
         """
@@ -38,23 +58,26 @@ class MovieInfoSource:
         to the movie on imdb.com, an image of the movie and 
         a basic description of the movie.
         """
-        # Get url of the movie at imdb.
-        movie_url = self.LINK_PREFIX + self.movies_info[movie_id][1] + '/'
-        
-        # Get content of the webpage.
-        usock = urlopen(movie_url)
-        data = usock.read()
-        usock.close()
+        try:
+            # Get url of the movie at imdb.
+            movie_url = self.LINK_PREFIX + self.movies_info[movie_id][1] + '/'
 
-        # Define custom parser that ignores errors when the html is non-valid.
-        parser = etree.XMLParser(recover=True)
-        # Parse html source using the parser.
-        root = etree.fromstring(data, parser=parser)
+            # Get content of the webpage.
+            usock = urlopen(movie_url)
+            data = usock.read()
+            usock.close()
 
-        # Obtain from the parsed html source movie description and image.
-        description = self._get_movie_description(root)
-        img = self._get_movie_image(root)
-        
+            # Define custom parser that ignores errors when the html is non-valid.
+            parser = etree.XMLParser(recover=True)
+            # Parse html source using the parser.
+            root = etree.fromstring(data, parser=parser)
+
+            # Obtain from the parsed html source movie description and image.
+            description = self._get_movie_description(root)
+            img = self._get_movie_image(root)
+        except Exception as e:
+            logger.warning(f'Failed to get information about moovie {movie_id} due to: {e}')
+            return '', '', None
         return description, movie_url, img
 
     def _get_movie_image(self, root):
